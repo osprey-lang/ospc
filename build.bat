a1af5d0a2d200f4ef0269d3ceef141841fddb21e
Adam Heurlin <adamheurlin@gmail.com>
2014-10-04 17:31:23 +0200

    Added build script templates.
    These are mentioned in .hgignore so that you can make local changes (fix up file paths etc.) without messing with anyone else's build scripts. A more sophisticated solution will be worked out later.

---
+++ build.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set OSPC=%OSP%\Osprey\bin\Release\Osprey.exe
+set LIB=%OSP%\lib
+
+if [%1]==[full] (
+	rem Generate error codes
+	cd osprey.compiler\errors
+
+	echo [!] Generating error code list...
+	call generateErrorCodes.bat
+	echo.
+
+	cd ..\..
+)
+
+echo [!] Compiling osprey.compiler...
+%OSPC% /libpath "%LIB%" /verbose /type module /out "%LIB%\osprey.compiler\osprey.compiler.ovm" /name osprey.compiler /doc "%LIB%\osprey.compiler\osprey.compiler.ovm.json" /formatjson osprey.compiler\osprey.compiler.osp && ^
+xcopy /Y /I osprey.compiler\errors\messages.txt "%LIB%\osprey.compiler" && ^
+echo. && ^
+echo [!] Compiling ospc... && ^
+%OSPC% /libpath "%LIB%" /verbose /main osprey.compiler.main /out bin\ospc.ovm /name ospc /doc bin\ospc.ovm.json /formatjson ospc\ospc.osp
---
+++ errorCodeGen/build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set OSPC=%OSP%\Osprey\bin\Release\Osprey.exe
+set LIB=%OSP%\lib
+
+%OSPC% /libpath "%LIB%" /verbose /main osprey.errorCodeGen.main errorCodeGen.osp
