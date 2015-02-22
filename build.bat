cce31008c512ae0b453632ac197ad777b428c578
Adam Heurlin <Heurlinius@users.noreply.github.com>
2015-02-22 22:57:21 +0100

    [osprey.compiler] * Add basic wrapped parse tree implementation * Include wrapped parse tree in build * Minor fixes
    [general]
    * Improvements in build.bat

--- build.bat
+++ build.bat
@@ -18,7 +18,10 @@ if [%1]==[full] (
 
 echo [!] Compiling osprey.compiler...
 %OSPC% /libpath "%LIB%" /verbose /type module /out "%LIB%\osprey.compiler\osprey.compiler.ovm" /name osprey.compiler /doc "%LIB%\osprey.compiler\osprey.compiler.ovm.json" /formatjson osprey.compiler\osprey.compiler.osp && ^
-xcopy /Y /I osprey.compiler\errors\messages.txt "%LIB%\osprey.compiler" && ^
-echo. && ^
-echo [!] Compiling ospc... && ^
-%OSPC% /libpath "%LIB%" /verbose /main osprey.compiler.main /out bin\ospc.ovm /name ospc /doc bin\ospc.ovm.json /formatjson ospc\ospc.osp
+xcopy /Y /I osprey.compiler\errors\messages.txt "%LIB%\osprey.compiler"
+
+if %ERRORLEVEL%==0 (
+	echo.
+	echo [!] Compiling ospc...
+	%OSPC% /libpath "%LIB%" /verbose /main osprey.compiler.main /out bin\ospc.ovm /name ospc /doc bin\ospc.ovm.json /formatjson ospc\ospc.osp
+)
