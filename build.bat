d7c4c0a56bda836665389b7db4b0ae5fd377ab4b
Adam Heurlin <Heurlinius@users.noreply.github.com>
2015-02-27 07:59:44 +0100

    * Ensure every file ends with a newline * Minor improvement to build script
    [errorCodeGen]
    * Minor improvement to build script

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Path to the compiler
-set OSPC=%OSP%\Osprey\bin\Release\Osprey.exe
+set OSPC="%OSP%\Osprey\bin\Release\Osprey.exe"
 rem Path to the library folder
 set LIB=%OSP%\lib
 
